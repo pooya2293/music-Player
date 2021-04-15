0dd1e70dd132c51d4773e56300f07c8734682ded
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-04-15 12:15:03 +0430

    done show duration Time  - and solve showing NaN Error --> 00:00

--- script.js
+++ script.js
@@ -72,17 +72,18 @@ function setProgress(e) {
 	audio.currentTime = (clickX / width) * duration;	
 }
 
-function DurTime (e){
+function DurTime (e) {
 	const {duration,currentTime} = e.srcElement;
-	let sec;
+	var sec;
+	var sec_d;
 
-	// define minutes
+	// define minutes currentTime
 	let min = (currentTime==null)? 0:
 	 Math.floor(currentTime/60);
 	 min = min <10 ? '0'+min:min;
 
-	// define seconds
-	function get_sec (x){
+	// define seconds currentTime
+	function get_sec (x) {
 		if(Math.floor(x) >= 60){
 			
 			for (var i = 1; i<=60; i++){
@@ -97,9 +98,39 @@ function DurTime (e){
 		 }
 	} 
 
-	get_sec (currentTime);
+	get_sec (currentTime,sec);
 
+	// change currentTime DOM
 	currTime.innerHTML = min +':'+ sec;
+
+	// define minutes duration
+	let min_d = (isNaN(duration) === true)? '0':
+		Math.floor(duration/60);
+	 min_d = min_d <10 ? '0'+min_d:min_d;
+
+
+	 function get_sec_d (x) {
+		if(Math.floor(x) >= 60){
+			
+			for (var i = 1; i<=60; i++){
+				if(Math.floor(x)>=(60*i) && Math.floor(x)<(60*(i+1))) {
+					sec_d = Math.floor(x) - (60*i);
+					sec_d = sec_d <10 ? '0'+sec_d:sec_d;
+				}
+			}
+		}else{
+		 	sec_d = (isNaN(duration) === true)? '0':
+		 	Math.floor(x);
+		 	sec_d = sec_d <10 ? '0'+sec_d:sec_d;
+		 }
+	} 
+
+	// define seconds duration
+	
+	get_sec_d (duration);
+
+	// change duration DOM
+	durTime.innerHTML = min_d +':'+ sec_d;
 		
 };
 
